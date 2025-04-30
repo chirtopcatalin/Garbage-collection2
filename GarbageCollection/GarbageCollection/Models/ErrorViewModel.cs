ed74a35ff65a1e20b76f793517fe6a7cd440ef71
Chirtop Catalin-Ioan <124921549+chirtopcatalin@users.noreply.github.com>
2025-04-30 23:16:10 +0300

    Added project files

---
+++ GarbageCollection/GarbageCollection/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GarbageCollection.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
